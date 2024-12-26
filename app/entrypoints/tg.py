372b1e9b2fc5d89cf7316e0a6397c60b39b1ac42
Muninn <ruihua@crestal.network>
2024-12-26 11:42:57 +0800

    refactor: tg import rel

--- app/entrypoints/tg.py
+++ app/entrypoints/tg.py
@@ -2,15 +2,42 @@ import asyncio
 import logging
 import signal
 import sys
-from os import getenv
 
-from app.config import config
-from app.db import init_db
+from sqlmodel import Session, select
+
+from app.config.config import config
+from app.models.db import init_db, get_engine, Agent
 from tg.bot.pool import BotPool
-from tg.schedule.agent import AgentScheduler
-
 
 logger = logging.getLogger(__name__)
+
+
+class AgentScheduler:
+    def __init__(self, bot_pool):
+        self.bot_pool = bot_pool
+
+    def check_new_bots(self):
+        with Session(get_engine()) as db:
+            # Get all telegram agents
+            agents = db.exec(
+                select(Agent).where(
+                    Agent.telegram_enabled == True,
+                )
+            ).all()
+
+            new_bots = []
+            for agent in agents:
+                if agent.telegram_config["token"] not in self.bot_pool.bots:
+                    new_bots.append(agent.telegram_config)
+            return new_bots
+
+    async def start(self, interval):
+        while True:
+            print("check for new bots...")
+            await asyncio.sleep(interval)
+            if self.check_new_bots() != None:
+                for new_bot in self.check_new_bots():
+                    await self.bot_pool.init_new_bot(new_bot["kind"], new_bot["token"])
 
 
 def run_telegram_server() -> None:
