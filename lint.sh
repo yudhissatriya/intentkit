5fd0f6ea5a02977132d7ce30b8f2ca499443ff66
Muninn <ruihua@crestal.network>
2024-12-27 22:55:03 +0800

    feat: twitter skills post and reply tweet

--- lint.sh
+++ lint.sh
@@ -1,3 +1,11 @@
-black .
-isort .
+echo "Running bandit..."
 bandit -r app/ skills/ skill_sets/ utils/ -ll
+
+echo "Running pylint..."
+pylint .
+
+echo "Running black..."
+black .
+
+echo "Running isort..."
+isort .
--- skills/twitter/__init__.py
+++ skills/twitter/__init__.py
@@ -1,11 +1,19 @@
+"""Twitter skills."""
+
 from tweepy import Client
 
 from skills.twitter.base import TwitterBaseTool
 from skills.twitter.get_mentions import TwitterGetMentions
+from skills.twitter.post_tweet import TwitterPostTweet
+from skills.twitter.reply_tweet import TwitterReplyTweet
 
 
 def get_twitter_skill(name: str, client: Client) -> TwitterBaseTool:
     if name == "get_mentions":
         return TwitterGetMentions(client=client)
+    elif name == "post_tweet":
+        return TwitterPostTweet(client=client)
+    elif name == "reply_tweet":
+        return TwitterReplyTweet(client=client)
     else:
         raise ValueError(f"Unknown Twitter skill: {name}")
