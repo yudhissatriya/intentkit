57fde0a3fd359016a706ff355067f1409ffdab47
Muninn <ruihua@crestal.network>
2025-02-21 11:10:36 +0800

    fix: sentry error sample rate

--- app/scheduler.py
+++ app/scheduler.py
@@ -20,6 +20,7 @@ logger = logging.getLogger(__name__)
 if config.sentry_dsn:
     sentry_sdk.init(
         dsn=config.sentry_dsn,
+        sample_rate=config.sentry_sample_rate,
         traces_sample_rate=config.sentry_traces_sample_rate,
         profiles_sample_rate=config.sentry_profiles_sample_rate,
         environment=config.env,
