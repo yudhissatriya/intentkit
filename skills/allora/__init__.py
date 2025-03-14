e22da3ca7b8fc64f4ede9822ef8741d51cee6809
Muninn <ruihua@crestal.network>
2025-03-14 08:17:25 +0800

    fix: some skills

--- skills/allora/__init__.py
+++ skills/allora/__init__.py
@@ -53,9 +53,7 @@ def get_skills(
             available_skills.append(skill_name)
 
     # Get each skill using the cached getter
-    return [
-        get_allora_skill(name, config["api_key"], store) for name in available_skills
-    ]
+    return [get_allora_skill(name, store) for name in available_skills]
 
 
 def get_allora_skill(
