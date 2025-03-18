75fdf62761a7e4ad341cb323cadd7d931ff91b12
Muninn <ruihua@crestal.network>
2025-03-18 10:39:15 +0800

    fix: a import bug

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,4 +1,5 @@
 from app.admin.api import admin_router, admin_router_readonly
+from app.admin.credit import credit_router, credit_router_readonly
 from app.admin.health import health_router
 from app.admin.schema import schema_router_readonly
 
